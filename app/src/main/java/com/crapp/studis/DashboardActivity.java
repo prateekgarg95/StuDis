c73e897183e3767be3a6273fc357a211edc90e8b
Prateek Garg <prateekgarg95@gmail.com>
2015-07-12 19:13:38 +0530

    Initial commit

--- app/src/main/java/com/crapp/studis/DashboardActivity.java
+++ app/src/main/java/com/crapp/studis/DashboardActivity.java
@@ -11,15 +11,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -41,22 +41,16 @@ public class DashboardActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
-    private CollapsingToolbarLayout collapsingToolbar;
-
-    private int mutedColor;
-
     private DrawerLayout drawerLayout;
 
     private NavigationView navigationView;
+
+    private TextView txtName,txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        setupToolbar();
-        setupCollapsingToolbar();
-        setupNavigationView();
 
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.default_classroom_image);
         String imageDirectory = Environment.getExternalStorageDirectory().getPath() + "/TestApp";
@@ -75,8 +69,10 @@ public class DashboardActivity extends AppCompatActivity {
             }
         }
 
+        setupNavigationView();
 
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+        recyclerView = (RecyclerView)findViewById(R.id.classrooms_recycler_view);
 
         linearLayoutManager = new LinearLayoutManager(DashboardActivity.this);
         recyclerView.setLayoutManager(linearLayoutManager);
@@ -92,97 +88,66 @@ public class DashboardActivity extends AppCompatActivity {
         recyclerView.setAdapter(classroomRecyclerViewAdapter);
     }
 
-    private void setupToolbar(){
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        final ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
-
-    }
-
-    private void setupCollapsingToolbar(){
-        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle("Dashboard");
-        ImageView backdrop = (ImageView) findViewById(R.id.backdrop);
-        collapsingToolbar.setContentScrimColor(DashboardActivity.this.getResources().getColor(R.color.classroom_blue));
-
-        /*Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.backdrop_blue);
-        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(Palette palette) {
-                mutedColor = palette.getMutedColor(R.attr.colorPrimary);
-                collapsingToolbar.setContentScrimColor(mutedColor);
-            }
-        });*/
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
-        return true;
-    }
-
     private void setupNavigationView(){
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
         navigationView = (NavigationView)findViewById(R.id.navigation_view);
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 if (menuItem.isChecked()) menuItem.setChecked(false);
                 else menuItem.setChecked(true);
                 drawerLayout.closeDrawers();
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.profile:
-                        Toast.makeText(DashboardActivity.this,"Profile",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(DashboardActivity.this, "Profile", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.settings:
-                        Toast.makeText(DashboardActivity.this,"Settings",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(DashboardActivity.this, "Settings", Toast.LENGTH_SHORT).show();
                         return true;
                     default:
-                        Toast.makeText(DashboardActivity.this,"Fishy",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(DashboardActivity.this, "Fishy", Toast.LENGTH_SHORT).show();
                         return true;
                 }
             }
         });
-
-
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
 
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
-                super.onDrawerClosed(drawerView);
-            }
+        txtName = (TextView)findViewById(R.id.name_nav);
+        txtName.setText("Prateek Garg");
+        txtEmail = (TextView)findViewById(R.id.email_nav);
+        txtEmail.setText("prateekgarg95@gmail.com");
 
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
-
-                super.onDrawerOpened(drawerView);
-            }
-        };
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+        return true;
     }
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(Gravity.LEFT))
+        {
+            drawerLayout.closeDrawer(Gravity.LEFT);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
 
 
 }
